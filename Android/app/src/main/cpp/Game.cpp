9b7c7332bbbae9d4e8139672d5c609e20ca1f698
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-01-03 16:26:18 -0800

    added in Game.cpp file

---
+++ Android/app/src/main/cpp/Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dugch on 2025-01-03.
+//
+
+#include "Game.h"
---
+++ Android/app/src/main/cpp/Game.h
@@ -0,0 +1,14 @@
+//
+// Created by dugch on 2025-01-03.
+//
+
+#ifndef TINY_POTION_FACTORY_GAME_H
+#define TINY_POTION_FACTORY_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //TINY_POTION_FACTORY_GAME_H
