b658272f3953e87919e67075087ad42c7e47de67
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-01-03 16:26:05 -0800

    fixed main (sorta)

--- Android/app/src/main/cpp/main.cpp
+++ Android/app/src/main/cpp/main.cpp
@@ -1,6 +1,9 @@
 //
 // Created by dugch on 2025-01-03.
 //
+#include <game-activity/GameActivity.cpp>
+#include <game-text-input/gametextinput.cpp>
+
 extern "C" {
 //includes android c file to make android work (needed for app to work)
 #include <game-activity/native_app_glue/android_native_app_glue.c>
@@ -9,6 +12,13 @@ extern "C" {
 void handleAppCmd(android_app* app, int32_t cmd){
     //TODO
     LOGD("Processing app command:");
+
+    if(cmd == APP_CMD_INIT_WINDOW){
+        LOGD("initiated window");
+    }else if(cmd == APP_CMD_TERM_WINDOW){
+        LOGD("exiting app");
+    }
+
 }
 
 //app main file. acts as start of program (takes app as argument)
@@ -26,7 +36,7 @@ void android_main(android_app* app){
     //primary loop, runs while app is running
     do{
         //requests events from OS
-        if(ALooper_pollAll(0, nullptr, &events, (void**) &pollSource) > 0){
+        if(ALooper_pollOnce(0, nullptr, &events, (void**) &pollSource) >= 0){
             //processes the events as needed
             if(pollSource) pollSource->process(app, pollSource);
         }
