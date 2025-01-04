a423d08b11acf8ceb6cab574f9b123e5ce85278c
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-01-03 16:13:58 -0800

    added logging file

---
+++ Android/app/src/main/cpp/logging.h
@@ -0,0 +1,12 @@
+//
+// Created by dugch on 2025-01-03.
+//
+
+#ifndef TINY_POTION_FACTORY_LOGGING_H
+#define TINY_POTION_FACTORY_LOGGING_H
+
+#include <android/log.h>
+
+#define LOGD( ... ) __android_log_print(ANDROID_LOG_DEBUG, "LOG", __VA_ARGS__)
+
+#endif //TINY_POTION_FACTORY_LOGGING_H
