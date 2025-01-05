a2dfb690f8bfda91d35b30b58dee64096f1957cc
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-01-05 15:57:59 -0800

    removed old engine progress and add error log

--- Android/app/src/main/cpp/Game.h
+++ Android/app/src/main/cpp/Game.h
@@ -5,7 +5,7 @@
 #ifndef TINY_POTION_FACTORY_GAME_H
 #define TINY_POTION_FACTORY_GAME_H
 
-#include "Renderer.h"
+//#include "Renderer.h"
 
 #include <game-activity/native_app_glue/android_native_app_glue.h>
 
--- Android/app/src/main/cpp/logging.h
+++ Android/app/src/main/cpp/logging.h
@@ -9,4 +9,6 @@
 
 #define LOGD( ... ) __android_log_print(ANDROID_LOG_DEBUG, "LOG", __VA_ARGS__)
 
+#define LOGE( ... ) __android_log_print(ANDROID_LOG_DEBUG, "ERROR", __VA_ARGS__)
+
 #endif //TINY_POTION_FACTORY_LOGGING_H
