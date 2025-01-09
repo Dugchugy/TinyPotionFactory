1c08072ecd712640569eeaa81e96b32b6a20056c
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-01-08 18:14:04 -0800

    added Gobject class

---
+++ Android/app/src/main/cpp/Graphics/GObject.h
@@ -0,0 +1,17 @@
+#ifndef TINY_POTION_FACTORY_GRAPHIC_POSITION_H
+#define TINY_POTION_FACTORY_GRAPHIC_POSITION_H
+
+#include "Position.h"
+
+namespace GRAPHICS {
+
+    class GObject {
+    public:
+
+    private:
+        Position pos; 
+    }; // GObject
+
+} //GRAPHICS
+
+#endif
