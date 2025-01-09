e209355bdec16ff5ec646b74ae9fe7eb08f22e41
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-01-08 18:23:12 -0800

    added description for getPosition

--- Android/app/src/main/cpp/Graphics/GObject.h
+++ Android/app/src/main/cpp/Graphics/GObject.h
@@ -1,12 +1,15 @@
 #ifndef TINY_POTION_FACTORY_GRAPHIC_POSITION_H
 #define TINY_POTION_FACTORY_GRAPHIC_POSITION_H
 
-#include "Position.h"
+#include "Graphics/Position.h"
 
 namespace GRAPHICS {
 
     class GObject {
     public:
+        /// @brief gets a pointer to the current position of the object
+        /// @return the current psotion pointer
+        Position* getPosition() {return &this->pos;}
 
     private:
         Position pos; 
