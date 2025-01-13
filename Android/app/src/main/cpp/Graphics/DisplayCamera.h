eede91da898f1e4a5114f8d0d65ee28e1087b0a1
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-01-12 16:26:05 -0800

    added display camera file

---
+++ Android/app/src/main/cpp/Graphics/DisplayCamera.h
@@ -0,0 +1,14 @@
+#ifndef TINY_POTION_FACTORY_GRAPHIC_IMAGE_H
+#define TINY_POTION_FACTORY_GRAPHIC_IMAGE_H
+
+#include "Graphics/GObject.h"
+
+namespace GRAPHICS {
+
+    class DisplayCamera : public GObject {
+
+    }; //DisplayCamera
+
+} //GRAPHICS
+
+#endif
