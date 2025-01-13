ae1ed21f13009fb6a94f64a146b4a914cad16960
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-01-12 17:12:48 -0800

    added function headers to image store

--- Android/app/src/main/cpp/Graphics/DisplayCamera.h
+++ Android/app/src/main/cpp/Graphics/DisplayCamera.h
@@ -10,6 +10,8 @@ namespace GRAPHICS {
     /// @brief creats a camera used to render things on to the screen. only one should be needed
     class DisplayCamera : public GObject {
 
+    public:
+
         /// @brief Draws the passed image on the screen at the passed position
         /// @param pos the position of the image, position is in world space.
         /// @param i the image to draw.
--- Android/app/src/main/cpp/Graphics/ImageStore.h
+++ Android/app/src/main/cpp/Graphics/ImageStore.h
@@ -1,11 +1,34 @@
 #ifndef TINY_POTION_FACTORY_GRAPHIC_IMAGE_STORE_H
 #define TINY_POTION_FACTORY_GRAPHIC_IMAGE_STORE_H
 
+#include <unordered_map>
+#include <string>
+
+#include "Graphics/Image.h"
+
 namespace GRAPHICS{
 
     class ImageStore {
-        
-    }
+
+    public:
+
+        /// @brief loads an image from the resouce stores with the specified path
+        /// @param path the path to find the iamge to load
+        /// @return the image found at the passed path (may be loaded)
+        Image* load_image_from_path(std::string path);
+
+    protected:
+
+        /// @brief clears all mappings stored in the image store, functionall reseting it. very unsafe
+        void Clear();
+
+
+    private:
+
+        /// @brief stores the map of all images loaded with the image store
+        std::unordered_map<std::string, Image> ImageSet;
+
+    };
 
 } // GRAPHICS
 
