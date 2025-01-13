abd20e2bfe63fc5b989a9f5507495aac4af648da
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-01-12 16:46:18 -0800

    corrected description

--- Android/app/src/main/cpp/Graphics/DisplayCamera.h
+++ Android/app/src/main/cpp/Graphics/DisplayCamera.h
@@ -10,7 +10,7 @@ namespace GRAPHICS {
     /// @brief creats a camera used to render things on to the screen. only one should be needed
     class DisplayCamera : public GObject {
 
-        /// @brief Draws teh passed image on the screen at the passed position
+        /// @brief Draws the passed image on the screen at the passed position
         /// @param pos the position of the image, position is in world space.
         /// @param i the image to draw.
         virtual void DrawImage(Position pos, Image i) = 0;
