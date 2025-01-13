3c9bf344527a50608e815e5ff037992ad1051246
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-01-12 16:44:16 -0800

    added the draw image function to the Display camera

--- Android/app/src/main/cpp/Graphics/DisplayCamera.h
+++ Android/app/src/main/cpp/Graphics/DisplayCamera.h
@@ -2,10 +2,18 @@
 #define TINY_POTION_FACTORY_GRAPHIC_IMAGE_H
 
 #include "Graphics/GObject.h"
+#include "Graphics/Position.h"
+#include "Graphics/Image.h"
 
 namespace GRAPHICS {
 
+    /// @brief creats a camera used to render things on to the screen. only one should be needed
     class DisplayCamera : public GObject {
+
+        /// @brief Draws teh passed image on the screen at the passed position
+        /// @param pos the position of the image, position is in world space.
+        /// @param i the image to draw.
+        virtual void DrawImage(Position pos, Image i) = 0;
 
     }; //DisplayCamera
 
