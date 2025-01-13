648ad89aa49218af705ac1927f6f56555c119c43
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-01-12 16:13:30 -0800

    added functions to Image

--- Android/app/src/main/cpp/Graphics/Image.h
+++ Android/app/src/main/cpp/Graphics/Image.h
@@ -1,9 +1,24 @@
 #ifndef TINY_POTION_FACTORY_GRAPHIC_IMAGE_H
 #define TINY_POTION_FACTORY_GRAPHIC_IMAGE_H
 
+#include <string>
+
 namespace GRAPHICS{
 
+    /// @brief Acts as a holder for an image. must be defined for the specifc system being used
+    /// used by the camera to draw an object on the screen in 2D
     class Image{
+
+    public:
+        /// @brief default constructor, creates an empty image
+        Image(); 
+
+        /// @brief create a new image loaded from the specified filename in the resource path
+        /// @param filename the resource path to pull the image from
+        Image(std::string filename);
+
+        /// @brief Destroys the image after its deallocated.
+        ~Image();
 
     }; // Image
 
