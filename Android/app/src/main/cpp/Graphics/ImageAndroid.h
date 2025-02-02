f2dc9bd8cdd78d222f8c00ca768fd5088dfa44d3
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-02-02 15:59:06 -0800

    added androidImage class

---
+++ Android/app/src/main/cpp/Graphics/ImageAndroid.h
@@ -0,0 +1,15 @@
+#ifndef TINY_POTION_FACTORY_GRAPHIC_IMAGE_ANDROID_H
+#define TINY_POTION_FACTORY_GRAPHIC_IMAGE_ANDROID_H
+
+#include <string>
+#include "Graphics/Image.h"
+
+namespace GRAPHICS{
+
+    class ImageAndroid : public Image {
+
+    }; // ImageAndroid
+
+} // GRAPHICS
+
+#endif
