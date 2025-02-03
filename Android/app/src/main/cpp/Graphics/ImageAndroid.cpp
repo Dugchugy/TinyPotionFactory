21e593aff8247a09cdc0224c27f94b72765ad9b0
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-02-02 17:54:13 -0800

    added filename based constructor

--- Android/app/src/main/cpp/Graphics/ImageAndroid.cpp
+++ Android/app/src/main/cpp/Graphics/ImageAndroid.cpp
@@ -1,5 +1,48 @@
 #include "Graphics/ImageAndroid.h"
 
+#include <android/asset_manager.h>
+#include <GLES3/gl3.h>
+
+#include "stb_image.h"
+
+#include "logging.h"
+
 namespace GRAPHICS{
+
+    /// @brief create a new image loaded from the specified filename in the resource path
+    /// @param filename the resource path to pull the image from
+    ImageAndroid::ImageAndroid(AAssetManager *assetMan, const std::string& filename) {
+
+
+        //loads the texture from the assets
+        AAsset* asset = AAssetManager_open(assetMan,
+                                        filePath.c_str(),
+                                        AASSET_MODE_BUFFER);
+
+        //gets the length and buffer loaded
+        off_t length = AAsset_getLength(asset);
+        const void * buffer = AAsset_getBuffer(asset);
+
+        if(!buffer){
+            LOGE("failed to load asset at path %s", filePath.c_str());
+            AAsset_close(asset);
+            return;
+        }
+
+        //reads the image from the asset buffer
+        ImgData = stbi_load_from_memory((const uint8_t *) buffer, length,
+                                        &width, &height, nullptr, 4);
+
+        if(!ImgData){
+            LOGE("Failed to load image: %s", stbi_failure_reason());
+            return;
+        }
+
+    } // end of ImageAndroid::ImageAndroid(AAssetManager *assetMan, const std::string& filename)
+
+    ImageAndroid::~ImageAndroid() {
+        //frees the loaded image data
+        stbi_image_free(ImgData);
+    } // end of ImageAndroid::~ImageAndroid()
 
 } // GRAPHICS
--- Android/app/src/main/cpp/Graphics/ImageAndroid.h
+++ Android/app/src/main/cpp/Graphics/ImageAndroid.h
@@ -4,23 +4,34 @@
 #include <string>
 #include "Graphics/Image.h"
 
+#include <android/asset_manager.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 namespace GRAPHICS{
 
     class ImageAndroid : public Image {
 
+    public:
+
         /// @brief default constructor, creates an empty image
-        ImageAndroid() {}; 
+        ImageAndroid(); 
 
         /// @brief create a new image loaded from the specified filename in the resource path
         /// @param filename the resource path to pull the image from
-        ImageAndroid(const std::string& filename) {};
+        ImageAndroid(AAssetManager *assetMan, const std::string& filename);
 
         /// @brief Destroys the image after its deallocated.
-        ~ImageAndroid() {};
+        ~ImageAndroid();
 
         /// @brief sets up the texture to prepare for it to be rendered
         /// @return returns the ID that this texture can be referenced at.
-        virtual int SetupTexture() = 0;
+        virtual int SetupTexture();
+
+    private:
+
+        stbi_uc * ImgData;
 
     }; // ImageAndroid
 
