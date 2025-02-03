fb337522e7c6815e804a9665f802eafc882a3bf4
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-02-02 17:30:28 -0800

    added getters for width and height

--- Android/app/src/main/cpp/Graphics/Image.h
+++ Android/app/src/main/cpp/Graphics/Image.h
@@ -24,11 +24,19 @@ namespace GRAPHICS{
         /// @return returns the ID that this texture can be referenced at.
         virtual int SetupTexture() = 0;
 
+        /// @brief gets the width of the image in pixels
+        /// @return the images width in pixels
+        int getWidth() {return width;}
+
+        /// @brief gets the height of the image in pixels
+        /// @return the images height in pixels
+        int getHeight() {return height;}
+
     private: 
 
         //the width and heigh of the texture in pixels
         int width;
-        int heigh;
+        int height;
 
     }; // Image
 
