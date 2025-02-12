70bcb8af53d0ad7fbc3877a5e67915c6648e1f33
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-02-11 17:23:18 -0800

    added offset for GraphicsObject

--- Android/app/src/main/cpp/Graphics/GObject2D.h
+++ Android/app/src/main/cpp/Graphics/GObject2D.h
@@ -13,13 +13,17 @@ namespace GRAPHICS {
 
         /// @brief draws the GraphicsObject using the current camera
         /// @param cam the camera to draw the graphics object with
-        void Draw(DisplayCamera& cam){cam.DrawImage(*this->getPosition(), *this->im);}
+        void Draw(DisplayCamera& cam){
+            cam.DrawImage(*this->getPosition() - this->offset, *this->im);
+        }
 
 
     protected: 
 
         /// @brief a pointer to the iamge used by the graphic objects 2D
         Image* im;
+
+        Position offset;
 
     }; //GOBject 2D
 } // GRAPHCIS
