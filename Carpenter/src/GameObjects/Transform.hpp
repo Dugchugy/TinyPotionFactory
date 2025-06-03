b4b7979c7a1fd94f5c2820044aa183dda96e5299
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-03 08:30:39 -0700

    added constructors for transform

--- Carpenter/src/GameObjects/Transform.hpp
+++ Carpenter/src/GameObjects/Transform.hpp
@@ -2,6 +2,22 @@
 namespace PotionParts {
 
 class Transform {
+public:
+
+   /// @brief constructs a default transform with zero position and rotation and 1 scale
+   Transform();
+
+   /// @brief constructs a transform with the specified position
+   Transform( float position );
+
+   /// @brief constructs a transform with the specified position, rotation, and scale
+   Transform( float position, float rotation, float scale );
+
+private:
+
+   Vector3 _position;
+   Vector3 _rotation;
+   Vector3 _scale;
 
 }; // transform
 
