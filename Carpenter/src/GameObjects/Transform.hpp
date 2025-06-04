7fbe9725a27a972bcc3fb24247c1f19f8baf393f
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-03 17:55:37 -0700

    added getters for position rotation and scale

--- Carpenter/src/GameObjects/Transform.hpp
+++ Carpenter/src/GameObjects/Transform.hpp
@@ -13,6 +13,15 @@ public:
    /// @brief constructs a transform with the specified position, rotation, and scale
    Transform( float position, float rotation, float scale );
 
+   /// @brief returns the current position
+   Vector3& position();
+
+   /// @brief returns the current rotation
+   Vector3& rotation();
+
+   /// @brief returns the current scale
+   Vector3& scale()
+
 private:
 
    Vector3 _position;
