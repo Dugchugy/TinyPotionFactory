c5662c7b23f0d30205c9d7687ead21848a3305fc
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-05 08:33:24 -0700

    started model class

---
+++ Carpenter/src/GameObjects/Model.hpp
@@ -0,0 +1,16 @@
+#include "Transform.hpp"
+#include <Graphics/mesh.hpp>
+#include <Graphics/texture.hpp>
+
+namespace PotionParts {
+
+class Model {
+public:
+
+private:
+
+   
+
+}; //Model
+
+} //PotionParts
