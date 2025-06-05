bbc465cbd226dffd83caea1207c91659c00fab0e
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-05 08:28:24 -0700

    added the getters for transform

--- Carpenter/src/GameObjects/Transform.cpp
+++ Carpenter/src/GameObjects/Transform.cpp
@@ -7,3 +7,15 @@ Transform::Transform( Vector3 position, Vector3 rotation, Vector3 scale ) {
    _rotation = rotation;
    _scale = scale;
 }
+
+Vector3& Transform::position() {
+   return _position;
+}
+
+Vector3& Transform::rotation() {
+   return _rotation;
+}
+
+Vector3& Transform::scale() {
+   return _scale;
+}
