717746095451e01a18f05a082bef7e9d6f7048b4
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-05 08:25:24 -0700

    implemented Transform constructor

---
+++ Carpenter/src/GameObjects/Transform.cpp
@@ -0,0 +1,9 @@
+#include "Transform.hpp"
+
+using namespace PotionParts;
+
+Transform::Transform( Vector3 position, Vector3 rotation, Vector3 scale ) {
+   _posotion = position;
+   _rotation = rotation;
+   _scale = scale;
+}
