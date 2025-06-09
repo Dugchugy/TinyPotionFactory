391e2b7eb6c0bc6607d641566bec0be78e06ab5a
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-09 08:04:18 -0700

    modified GameObject3D to use new model and transform constructirs

--- Carpenter/src/GameObjects/GameObject3D.cpp
+++ Carpenter/src/GameObjects/GameObject3D.cpp
@@ -2,8 +2,7 @@
 
 using namespace PotionParts;
 
-GameObject3D::GameObject3D(Engine::Graphics::Mesh newMesh, Engine::Graphics::Texture texture) :
-    GameObject("a 3d object"), text(texture), mesh(newMesh) {}
+GameObject3D::GameObject3D(Engine::Graphics::Mesh newMesh, Engine::Graphics::Texture texture) : model( mesh, text ), transform()  {}
 
 void GameObject3D::draw(Engine::Graphics::Renderer renderer) {
     model.draw( renderer, transform );
