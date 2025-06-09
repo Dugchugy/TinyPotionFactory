1b8f5b5882c48d723766a157b1abd32dde5e2fc1
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-09 08:01:53 -0700

    implemented model constructor

--- Carpenter/src/GameObjects/Model.cpp
+++ Carpenter/src/GameObjects/Model.cpp
@@ -3,7 +3,12 @@
 
 using namespace PotionParts;
 
-Model::draw(Engine::Graphics::Renderer renderer, Transform transform) {
+Model::Model( Engine::Graphics::Mesh m, Engine::Graphics::Texture t ) {
+   mesh = m;
+   text = t;
+}
+
+void Model::draw(Engine::Graphics::Renderer renderer, Transform transform) {
    renderer.UseTexture( text, GL_TEXTURE0 );
    renderer.DrawMesh( &mesh, transform.position(), transform.rotation(), transform.scale() );
 }
