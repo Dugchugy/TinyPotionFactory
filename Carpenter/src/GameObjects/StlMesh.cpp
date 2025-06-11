f680ff7d35c795b3d7dcdb731857184c26a1772c
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-10 18:34:05 -0700

    started implementing loading STL files

--- Carpenter/src/GameObjects/StlMesh.cpp
+++ Carpenter/src/GameObjects/StlMesh.cpp
@@ -6,4 +6,30 @@ using namespace PotionParts;
 
 StlMesh::StlMesh( Std::String filename ){
 
+   void* fileContent = nullptr;
+
+   int fileSize = 0;
+   int fileError = 0;
+
+   emscripten_mget_data( filename, &fileContent, &fileSize, &fileError );
+
+   if ( fileError ) {
+      //throw error
+   }
+
+   // skips the 80 byte header and 4 byte triangle count
+   int i = 84;
+
+   while ( i < fileSize ) {
+      if( fileSize - i >= 50 ) {
+         // first 12 are the normal (calculated by mesh) so they are ignored
+         
+         i += 50;
+      } else {
+         i = fileSize;
+      }
+   }
+
+   free( fileContent );
+
 } //StlMesh( Std::String )
