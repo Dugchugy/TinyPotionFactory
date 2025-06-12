c35f34952834046d57d2b9cd1e097d2e55043f2f
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-12 08:29:19 -0700

    added memory operations for AssetStream

--- Carpenter/src/GameObjects/AssetStream.cpp
+++ Carpenter/src/GameObjects/AssetStream.cpp
@@ -4,6 +4,37 @@
 
 using namespace PotionParts;
 
-AssetStream::AssetStream( std::string filename ) {
+AssetStream::AssetStream( std::string path ) {
+   _asset = nullptr;
+   _path = path;
+}
 
+AssetStream::~AssetStream() {
+   if ( _asset != nullptr ) {
+      free( _asset );
+      _asset = nullptr;
+   }
+}
+
+AssetStream::AssetStream( const AssetStream& a ) {
+   if ( _asset != nullptr ) {
+      free( _asset );
+      _asset = nullptr;
+   }
+
+   memcpy( &a._asset, &_asset );
+
+   _path = a._path;
+}
+
+const &AssetStream AssetStream::operator= ( const &AssetStream a ) {
+   cpy = AssetStream( a );
+
+   void* tmp = cpy._asset;
+   cpy._asset = _asset;
+   _asset = tmp;
+
+   _path = a._path;
+
+   return a;
 }
