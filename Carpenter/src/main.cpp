cdbe6946dc945d9ab7849de379187ee98f41fc09
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-15 13:10:00 -0700

    added caldren to main

--- Carpenter/src/main.cpp
+++ Carpenter/src/main.cpp
@@ -8,13 +8,13 @@ using namespace Engine;
 class ExampleScene : public Scene {
   private:
   PotionParts::GameObject3D g3D;
+  PotionParts::GameObject3D cald;
   float time = 0;
   UI::UILabel label;
 
   public:
   ExampleScene() : Scene("ExampleScene"), 
-    g3D(PotionParts::GameObject3D(Graphics::Cube(), Graphics::Texture("Assets/placeholder.png"), 
-    PotionParts::Transform(PotionParts::Vector3(0, 0, 10)))), label("Label", "Hello World") {
+    g3D(PotionParts::GameObject3D( Graphics::Cube(), Graphics::Texture( "Assets/placeholder.png" ), PotionParts::Transform( PotionParts::Vector3( 0, 0, 10 ) ) ) ), label("Label", "Hello World" ), cald( PotionParts::GameObject3D( PotionParts::StMesh( "Assets/Caldren.stl" ), Graphics::Texture( "Assets/placeholder.png" ), PotionParts::Transform( PotionParts::Vector3( 0, 2, 10 ) ) ) ) {
        AddChild(&label);
     }
 
@@ -24,6 +24,7 @@ class ExampleScene : public Scene {
     g3D.transform.rotation().yIs(time);
 
     g3D.draw(Game::getInstance().GetRenderer());
+    cald draw(Game::getInstance().GetRenderer());
   }
 
   void Update(float dt) override {
