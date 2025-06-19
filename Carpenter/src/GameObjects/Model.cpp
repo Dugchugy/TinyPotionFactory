a9ad6b36ea5bd353f98153b51a374e92feeb910b
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-19 08:28:51 -0700

    added addMesh function

--- Carpenter/src/GameObjects/Model.cpp
+++ Carpenter/src/GameObjects/Model.cpp
@@ -11,6 +11,11 @@ Model::Model( Engine::Graphics::Mesh m, Engine::Graphics::Texture t ) : Model() 
    addMesh( m, t );
 }
 
+void Model::addMesh( Engine::Graphics::Mesh m, Engine::Graphics::Texture t ) {
+   subMeshs.push( m );
+   textures.push( t );
+}
+
 void Model::draw(Engine::Graphics::Renderer renderer, Transform transform) {
    for ( int i = 0; i < subMeshs.length(); i++ ) {
       renderer.UseTexture( textures[ i ], GL_TEXTURE0 );
