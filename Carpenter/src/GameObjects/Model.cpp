4b95e9aa84080809dd31b14d25201330f4c44fa5
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-20 08:21:35 -0700

    Update model.cpp to match header

--- Carpenter/src/GameObjects/Model.cpp
+++ Carpenter/src/GameObjects/Model.cpp
@@ -3,20 +3,27 @@
 
 using namespace PotionParts;
 
-Model::Model(): 
+
+Model::Model( ModelBase* base ) : _base( base ) {}
+
+Model::Draw(Engine::Graphics::Renderer renderer, const Transform transform) const {
+   _base->Draw( renderer, transform );
+}
+
+ModelBase::ModelBase(): 
    subMeshs( std::vector<Engine::Graphics::Mesh>() ),
    textures(std::vector<Engine:: Graphics::Texture>() ) {}
 
-Model::Model( Engine::Graphics::Mesh m, Engine::Graphics::Texture t ) : Model() {
+ModelBase::ModelBase( Engine::Graphics::Mesh m, Engine::Graphics::Texture t ) : Model() {
    addMesh( m, t );
 }
 
-void Model::addMesh( Engine::Graphics::Mesh m, Engine::Graphics::Texture t ) {
+void ModelBase::addMesh( Engine::Graphics::Mesh m, Engine::Graphics::Texture t ) {
    subMeshs.push( m );
    textures.push( t );
 }
 
-void Model::draw(Engine::Graphics::Renderer renderer, Transform transform) {
+void ModelBase::draw(Engine::Graphics::Renderer renderer, const Transform transform) const {
    for ( int i = 0; i < subMeshs.length(); i++ ) {
       renderer.UseTexture( textures[ i ], GL_TEXTURE0 );
       renderer.DrawMesh( &subMeshs[ i ], 
