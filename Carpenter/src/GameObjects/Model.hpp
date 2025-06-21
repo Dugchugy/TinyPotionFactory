1dd73f728119997502803a80272e7f2d00af90a0
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-21 07:42:52 -0700

    made manager a pointer

--- Carpenter/src/GameObjects/Model.hpp
+++ Carpenter/src/GameObjects/Model.hpp
@@ -59,7 +59,7 @@ private:
    /// @brief checks if a model has been loaded before and returns a pointer to it if it has
    ModelBase* checkLoaded( std::string filename )
 
-   static ModelManager manager;
+   static ModelManager * manager;
 
    std::unordered_map<std::string, ModelBase> modelMap;
 
