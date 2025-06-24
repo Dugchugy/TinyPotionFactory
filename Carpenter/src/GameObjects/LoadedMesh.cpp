e7539dd50b555482e4aceeb84f597e26efb7631e
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-23 22:28:32 -0700

    fixed errors in loadedMesh

--- Carpenter/src/GameObjects/LoadedMesh.cpp
+++ Carpenter/src/GameObjects/LoadedMesh.cpp
@@ -3,7 +3,7 @@
 using namespace PotionParts;
 
 LoadedMesh::LoadedMesh( std::vector<Tri> triangles ) {
-   for ( int i = 0; i < triangles.size; i++ ) {
+   for ( int i = 0; i < triangles.size(); i++ ) {
       AddTriangle( triangles[i].v1, triangles[i].v2, triangles[i].v3 );
       AddTriangle( triangles[i].v3, triangles[i].v2, triangles[i].v1 );
    }
--- Carpenter/src/GameObjects/LoadedMesh.hpp
+++ Carpenter/src/GameObjects/LoadedMesh.hpp
@@ -1,5 +1,5 @@
-#idndef LOADEDMESH_HPP
-#define LOADEDNESH_HPP
+#ifndef LOADEDMESH_HPP
+#define LOADEDMESH_HPP
 
 #include<Graphics/Mesh.hpp>
 #include<vector>
@@ -7,18 +7,18 @@
 namespace PotionParts {
 
 struct Tri {
-   Engine::Graphics::Vertex v1
-   Engine::Graphics::Vertex v2
-   Engine::Graphics::Vertex v3
-}
+   Engine::Graphics::Vertex v1;
+   Engine::Graphics::Vertex v2;
+   Engine::Graphics::Vertex v3;
+}; //Tri
 
 class LoadedMesh : public Engine::Graphics::Mesh {
 
    /// @brief creates a mesh from an array of triangles
    LoadedMesh( std::vector<Tri> triangles );
 
-}
+}; //LoadedMesh
 
-};
+}
 
 #endif //LOADEDMESH_HPP
