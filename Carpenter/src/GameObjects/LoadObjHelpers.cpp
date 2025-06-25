e6096e585adb85a38c0a39f27a8b18bd95ea6ecb
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-24 20:53:34 -0700

    added splitString function

--- Carpenter/src/GameObjects/LoadObjHelpers.cpp
+++ Carpenter/src/GameObjects/LoadObjHelpers.cpp
@@ -2,6 +2,25 @@
 
 using namespace PotionParts;
 
+std::vector<std::string> splitString( const std::string & str, char c ){
+   std::vector<std::string> subStrs;
+
+   std::string currStr = "";
+
+   for ( int i = 0; i < str.size(); i++ ){
+      if ( str[i] == c ) {
+         subStrs.push_back( currStr );
+         currStr = "";
+      } else {
+         subStrs += str[i]
+      }
+   }
+
+   subStrs.push_back( currStr );
+
+   return subStrs;
+}
+
 TexCoords operator+( const TexCoords & x, const TexCoords & y ) {
    return { x.u + y.u, x.v + y.v };
 }
