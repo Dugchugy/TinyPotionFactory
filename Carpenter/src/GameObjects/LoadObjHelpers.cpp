9b7910c687bac674fe98ae450ca67170aaa388c4
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-06-24 22:39:22 -0700

    implemented parseFace

--- Carpenter/src/GameObjects/LoadObjHelpers.cpp
+++ Carpenter/src/GameObjects/LoadObjHelpers.cpp
@@ -12,7 +12,7 @@ std::vector<std::string> splitString( const std::string & str, char c ){
          subStrs.push_back( currStr );
          currStr = "";
       } else {
-         subStrs += str[i]
+         subStrs = subStrs + str[i];
       }
    }
 
@@ -72,6 +72,84 @@ Vector3 parseNormal( std::string & line ) {
    float z = stof( split[3], nullptr );
 
    return Vector3( x, y, z );
+}
+
+std::vector<Tri> parseFace( std::string & line, const std::vector<Vector3> & vertexes,
+                            const std::vector<TexCoords> & uVs ) {
+   std::vector<std::string> split = splitString( line, ' ' );
+
+   if ( split[0] != "f" ) {
+      throw InvalidLineException();
+   }
+
+   if ( split.size() < 4 ) {
+      // must have at least 3 verticies for a face (triangle)
+      throw InvalidLineException();
+   }
+
+   std::vector<Tri> result;
+
+   std::vector<Vector3> faceVertexes;
+   std::vector<TexCoords> faceUvs;
+
+   Vector3 avgVert = Vector3( 0, 0, 0 );
+   TexCoords avgTex = { 0, 0 };
+
+   for ( int i = 1; i < split.size(); i++ ) {
+      std::vector<std::string> indicies = splitString( split[i], '/' );
+
+      int vertIndex = stoi( indicies[0], nullptr );
+      int uvIndex = stoi( indicies[1], nullptr ); 
+
+      faceVertexes.push_back( vertexes[ vertIndex ] );
+      faceUvs.push_back( uVs[ uvIndex ] );
+
+      avgVert = avgVert + vertexes[ vertIndex ];
+      avgTex = avgTex + uVs[ uvIndex ];
+   }
+   float frac = 1.0f / ( split.size() - 1 );
+
+   avgVert = avgVert * frac;
+   avgTex = { avgTex.u * frac, avgTex.v * frac };
+
+   if ( split.size() == 4 ) {
+      result.push_back( {
+         faceVertexes[ 0 ].toVertex( faceUvs[ 0 ].u, faceUvs[ 0 ].v ),
+         faceVertexes[ 1 ].toVertex( faceUvs[ 1 ].u, faceUvs[ 1 ].v ),
+         faceVertexes[ 2 ].toVertex( faceUvs[ 2 ].u, faceUvs[ 2 ].v )
+      } );
+   } else if ( split.size() == 5 ) {
+      // adds a quad (4 vertices)
+      result.push_back( {
+         faceVertexes[ 0 ].toVertex( faceUvs[ 0 ].u, faceUvs[ 0 ].v ),
+         faceVertexes[ 1 ].toVertex( faceUvs[ 1 ].u, faceUvs[ 1 ].v ),
+         faceVertexes[ 2 ].toVertex( faceUvs[ 2 ].u, faceUvs[ 2 ].v )
+      } );
+      result.push_back( {
+         faceVertexes[ 2 ].toVertex( faceUvs[ 2 ].u, faceUvs[ 2 ].v ),
+         faceVertexes[ 3 ].toVertex( faceUvs[ 3 ].u, faceUvs[ 3 ].v ),
+         faceVertexes[ 0 ].toVertex( faceUvs[ 0 ].u, faceUvs[ 0 ].v )
+      } );
+   } else {
+      //uses the calculated average vertex as a middle point for a face
+      for ( int i = 1; i < split.size(); i++ ) {
+         if ( i == ( split.size() - 1 ) ) {
+            result.push_back( {
+               faceVertexes[ i - 1 ].toVertex( faceUvs[ i - 1 ].u, faceUvs[ i - 1 ].v ),
+               faceVertexes[ 0 ].toVertex( faceUvs[ 0 ].u, faceUvs[ 0 ].v ),
+               avgVert.toVertex( avgTex.u, avgTex.v )
+            } );
+         } else {
+            result.push_back( {
+               faceVertexes[ i - 1 ].toVertex( faceUvs[ i - 1 ].u, faceUvs[ i - 1 ].v ),
+               faceVertexes[ i ].toVertex( faceUvs[ i ].u, faceUvs[ i ].v ),
+               avgVert.toVertex( avgTex.u, avgTex.v )
+            } );
+         }
+      }
+   }
+
+   return result;
 }
 
 TexCoords operator+( const TexCoords & x, const TexCoords & y ) {
