3ea9c75dbaa7ef06afdcbd4dc05423c0041daf1e
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-07-22 08:29:50 -0700

    Update GameObject3D.cpp to use modellink

--- Carpenter/src/GameObjects/GameObject3D.cpp
+++ Carpenter/src/GameObjects/GameObject3D.cpp
@@ -2,7 +2,7 @@
 
 using namespace PotionParts;
 
-GameObject3D::GameObject3D(Transform t, Model m) : model(m), transform(t)  {}
+GameObject3D::GameObject3D(Transform t, ModelLink m) : model(m), transform(t)  {}
 
 void GameObject3D::draw(Engine::Graphics::Renderer renderer) {
     model.draw( renderer, transform );
