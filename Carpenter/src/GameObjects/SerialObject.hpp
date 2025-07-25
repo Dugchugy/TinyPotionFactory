8f54cd4eb400c02c5691484fe35f3455aae71cbd
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-07-25 08:23:08 -0700

    Create SerialObject.hpp

---
+++ Carpenter/src/GameObjects/SerialObject.hpp
@@ -0,0 +1,11 @@
+#ifndef SERIALOBJECT_HPP
+#define SERIALOBJECT_HPP
+
+/**
+ * stores on object that can be serialized and written into a file
+ */
+class SerialObject : public GameObject3D {
+   
+}
+
+#endif //SERIALOBJECT_HPP
