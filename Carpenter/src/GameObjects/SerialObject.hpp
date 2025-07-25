0c4659dc0ee689b12ce5c3be14862fa9f034a00f
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-07-25 08:30:09 -0700

    added constructor for SerialObject

--- Carpenter/src/GameObjects/SerialObject.hpp
+++ Carpenter/src/GameObjects/SerialObject.hpp
@@ -1,11 +1,16 @@
 #ifndef SERIALOBJECT_HPP
 #define SERIALOBJECT_HPP
 
+#include "GameObject3D.hpp"
+#include "Transform.hpp"
+#include "Model.hpp"
+
 /**
  * stores on object that can be serialized and written into a file
  */
 class SerialObject : public GameObject3D {
-   
+public:
+   SerialObject( Transform t, ModelLink m ) : GameObject3D( t, m ) {}
 }
 
 #endif //SERIALOBJECT_HPP
