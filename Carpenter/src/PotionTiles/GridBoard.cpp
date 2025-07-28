01a9eebb749b8d18443a1a6c37662fc846c05461
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-07-27 17:30:47 -0700

    added gribBoard.cpp

---
+++ Carpenter/src/PotionTiles/GridBoard.cpp
@@ -0,0 +1,11 @@
+#include "GridBoard.hpp"
+
+using namespace PotionGrid;
+
+void GridBoard::draw( Engine::Graphics::Renderer& renderer ) {
+    // calls draw on subGrids
+}
+
+void GridBoard::update( float timeSinceLastUpdate ) {
+    // calls update on subGrids for each phase
+}
