7e6a5e6e9744d562c1277c5f217131f31e1563e2
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-07-28 08:04:42 -0700

    added basic functions to TilePosition

--- Carpenter/src/PotionTiles/TilePosition.hpp
+++ Carpenter/src/PotionTiles/TilePosition.hpp
@@ -4,6 +4,24 @@
 namespace PotionGrid {
 
 class TilePosition {
+public:
+
+   /**
+    * @brief constructs tile position ( 0, 0 )
+    */
+   TilePosition ();
+
+   /**
+    * @brief constructs tile position ( x，y )
+    * @param x x
+    * @param y y
+    */
+   TilePosition( int x, int y );
+
+
+private:
+   int _x;
+   int _y;
 
 }; // TilePosition
 
