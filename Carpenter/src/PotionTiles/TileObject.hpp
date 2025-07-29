5f218dd207a7fc321881da8b41688849122c939e
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-07-29 08:44:09 -0700

    added Position for Tiles

--- Carpenter/src/PotionTiles/TileObject.hpp
+++ Carpenter/src/PotionTiles/TileObject.hpp
@@ -1,9 +1,14 @@
 #ifndef TILEOBJECT_HPP
 #define TILEOBJECT_HPP
 
+#include "TilePosition.hpp"
+
 namespace PotionGrid {
 
 class TileObject : public SerialObject {
+
+private:
+   TilePosition position;
 
 }; //TileObject
 
