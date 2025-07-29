d4c7da54782c61ff305e08a6097f1df5e2097f57
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-07-29 08:40:06 -0700

    started TileObject Header

---
+++ Carpenter/src/PotionTiles/TileObject.hpp
@@ -0,0 +1,12 @@
+#ifndef TILEOBJECT_HPP
+#define TILEOBJECT_HPP
+
+namespace PotionGrid {
+
+class TileObject : public SerialObject {
+
+}; //TileObject
+
+} //PotionGrid 
+
+#endif //TILEOBJECT_HPP
