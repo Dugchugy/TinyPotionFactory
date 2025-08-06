6ff920c8f5eba9daa04efaa493aa501416c7424d
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-08-05 21:02:00 -0700

    added type function and constructor

---
+++ Carpenter/src/PotionTiles/TileObject.cpp
@@ -0,0 +1,11 @@
+#include "TileObject.hpp"
+
+using namespace PotionGrid;
+
+TileType TileObject::type() {
+    return TileType::NullTile;
+}
+
+TileObject::TileObject( TilePosition pos ) {
+    
+}
--- Carpenter/src/PotionTiles/TileObject.hpp
+++ Carpenter/src/PotionTiles/TileObject.hpp
@@ -1,14 +1,20 @@
 #ifndef TILEOBJECT_HPP
 #define TILEOBJECT_HPP
 
+#include "GameObjects/SerialObject.hpp"
 #include "TilePosition.hpp"
+#include "TileIdentifier.hpp"
 
 namespace PotionGrid {
 
 class TileObject : public SerialObject {
 
+   TileObject( TilePosition pos );
+
 private:
    TilePosition position;
+
+   virtual TileType type();
 
 }; //TileObject
 
