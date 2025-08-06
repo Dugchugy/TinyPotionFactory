ebe2020d982a4ab067135c76bbfcffc66d2aeb42
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-08-05 22:15:05 -0700

    added functions to store tile objects

--- Carpenter/src/PotionTiles/GridBoard.cpp
+++ Carpenter/src/PotionTiles/GridBoard.cpp
@@ -6,12 +6,32 @@
 using namespace PotionGrid;
 
 void GridBoard::draw( Engine::Graphics::Renderer& renderer ) {
-    // calls draw on subGrids
+   // calls draw on subGrids
 }
 
 void GridBoard::update( float timeSinceLastUpdate ) {
-    // calls update on subGrids for each phase
+   // calls update on subGrids for each phase
 }
 
 GridBoard::GridBoard() : SerialObject(
-    PotionParts::Transform(), PotionParts::ModelLink( nullptr ) ) {}
+   PotionParts::Transform(), PotionParts::ModelLink( nullptr ) ) {}
+
+TileObject* GridBoard::getTile( TileIdentifier id ){
+    if ( tileMap.contains( id ) ) {
+        return tileMap.at( id );
+    } else {
+        return nullptr;
+    }
+}
+
+void GridBoard::addTile( TileObject object, TileIdentifier id ) {
+    tileMap.insert_or_assign( id, object );
+}
+
+void delTile( TileIdentifier id ) {
+    if ( tileMap.contains( id ) ) {
+        TileObject* ptr = tileMap.at( id );
+        tileMap.erase( id );
+        delete ptr;
+    }
+}
--- Carpenter/src/PotionTiles/GridBoard.hpp
+++ Carpenter/src/PotionTiles/GridBoard.hpp
@@ -2,7 +2,11 @@
 #define GRIDBOARD_HPP
 
 #include "../GameObjects/SerialObject.hpp"
+#include "TileIdentifier.hpp"
+#include "TileObject.hpp"
+
 #include <Renderer.hpp>
+#include <unordered_map>
 
 namespace PotionGrid {
 
@@ -12,27 +16,37 @@ namespace PotionGrid {
 class GridBoard : PotionParts::SerialObject {
 public:
 
-    /**
-     * @brief constructs a new GribBoard with empty Grids
-     */
-    GridBoard();
+   /**
+    * @brief constructs a new GribBoard with empty Grids
+    */
+   GridBoard();
 
-    /**
-     * @brief Draws the game object using the appropriate renderer
-     * @param renderer the renderer that will be used to draw the object
-     * 
-     * calls draw in all of the subgrids stored in this gridBoard
-     */
-    virtual void draw( Engine::Graphics::Renderer& renderer ); 
+   /**
+    * @brief Draws the game object using the appropriate renderer
+    * @param renderer the renderer that will be used to draw the object
+    * 
+    * calls draw in all of the subgrids stored in this gridBoard
+    */
+   virtual void draw( Engine::Graphics::Renderer& renderer ); 
 
-    /**
-     * @brief updates the game object. used to implement a physics tick
-     * @param timeSinceLastUpdate the time it has been since the last update in seconds
-     * 
-     * splits the update into the 4 phase update and runs all 4 update phases on the
-     * subgrids stored in this object.
-     */
-    virtual void update( float timeSinceLastUpdate );
+   /**
+    * @brief updates the game object. used to implement a physics tick
+    * @param timeSinceLastUpdate the time it has been since the last update in seconds
+    * 
+    * splits the update into the 4 phase update and runs all 4 update phases on the
+    * subgrids stored in this object.
+    */
+   virtual void update( float timeSinceLastUpdate );
+
+   TileObject* getTile( TileIdentifier id );
+
+   void addTile( TileObject object, TileIdentifier id );
+
+   void delTile( TileIdentifier id );
+
+private:
+
+    std::unordered_map< TileIdentifier, TileObject* > tileMap;
 
 }; //GridBoard
 
