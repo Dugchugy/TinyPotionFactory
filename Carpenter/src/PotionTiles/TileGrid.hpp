4a9c79d97a764a79fd1438e45aeff15fea8e8d35
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-08-05 22:42:23 -0700

    started TileGrid Header

---
+++ Carpenter/src/PotionTiles/TileGrid.hpp
@@ -0,0 +1,30 @@
+#ifndef TILEGRID_HPP
+#define TILEGRID_HPP
+
+#define GRID_SIZE 16
+
+#include "GameObjects/SerialObject.hpp"
+
+#include "GridBoard.hpp"
+#include "TileObject.hpp"
+#include "TilePosition.hpp"
+
+namespace PotionGrid {
+
+class TileGrid : public PotionParts::SerialObject {
+
+public:
+   
+   TileGrid( TilePosition corner );
+
+private:
+
+    TilePosition _bottomCorner;
+
+    TileIdentifier[ GRID_SIZE * GRID_SIZE ];
+
+}; //TileGrid
+
+} //PotionGrid
+
+#endif //TILEGRID_HPP
