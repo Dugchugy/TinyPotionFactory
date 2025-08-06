b9d51ed5e8e4d3f1ee6101d1aeab75841cdb5084
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-08-05 20:06:52 -0700

    added getters for TileIdentifer

--- Carpenter/src/PotionTiles/TileIdentifier.cpp
+++ Carpenter/src/PotionTiles/TileIdentifier.cpp
@@ -11,3 +11,15 @@ TileIdentifier::TileIdentifier( TileType type ) {
    _type = type;
    _index = nextIndex();
 }
+
+TileType TileIdentifier::type() {
+   return _type;
+}
+
+unsigned long TileIdentifier::index() {
+   return _index;
+}
+
+unsigned long TileIdentifier::nextIndex() {
+   return _nextIndex;
+}
