3d256d26276df3a74b496174b53d20fde80ad24f
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-08-10 14:06:21 -0700

    implemented getIdentifier

--- Carpenter/src/PotionTiles/TileGrid.cpp
+++ Carpenter/src/PotionTiles/TileGrid.cpp
@@ -4,3 +4,16 @@ using namespace PotionGrid;
 
 TileGrid::TileGrid( TilePosition corner ) : SerialObject(
    PotionParts::Transform(), PotionParts::ModelLink( nullptr ) ) {}
+
+TileIdentifier TileGrid::getIdentifier( TilePosition pos ){
+
+   TilePosition normalPos = pos - _corner;
+
+   if ( normalPos.x() < 0 || normalPos.x() >= GRID_SIZE ||
+      normalPos.y() < 0 || normalPos.y() >= GRUD_SIZE ) {
+      return TileIdentifier();
+   } else {
+      return grid[ normalPos.y() * GRID_SIZE + normalPos.x() ];
+   }
+
+}
