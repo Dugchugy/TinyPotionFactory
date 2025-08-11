0fa7a07c34a9ab80707bd6b6ee8a80cd46a31f76
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-08-11 08:36:22 -0700

    implemented setIdentifier

--- Carpenter/src/PotionTiles/TileGrid.cpp
+++ Carpenter/src/PotionTiles/TileGrid.cpp
@@ -10,10 +10,21 @@ TileIdentifier TileGrid::getIdentifier( TilePosition pos ){
    TilePosition normalPos = pos - _bittomCorner;
 
    if ( normalPos.x() < 0 || normalPos.x() >= GRID_SIZE ||
-      normalPos.y() < 0 || normalPos.y() >= GRUD_SIZE ) {
+      normalPos.y() < 0 || normalPos.y() >= GRID_SIZE ) {
       return TileIdentifier();
    } else {
       return grid[ normalPos.y() * GRID_SIZE + normalPos.x() ];
    }
 
+}
+
+void TileGrid::setIdentifier( TilePosition pos, TileIdentifier id ) {
+   TilePosition normalPos = pos - _bottomCorner;
+
+   if ( normalPos.x() < 0 || normalPos.x() >= GRID_SIZE ||
+      normalPos.y() < 0 || normalPos.y() >= GRID_SIZE ) {
+
+   } else {
+      grid[ normalPos.y() * GRID_SIZE + normalPos.x() ] = id;
+   }
 }
