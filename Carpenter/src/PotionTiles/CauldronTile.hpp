df7ad386e124075e754ac30e4648ee3ced52fc36
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-08-12 08:28:52 -0700

    created CauldronTile class

---
+++ Carpenter/src/PotionTiles/CauldronTile.hpp
@@ -0,0 +1,14 @@
+#ifndef CAULDRONTILE_HPP
+#define CAULDRONTILE_HPP
+
+#include "TileObject.hpp"
+
+namespace PotionGrid {
+
+class CauldronTile : public TileObject {
+
+}; //CauldronTile 
+
+} //PotionGrid
+
+#endif //CAULDRONTILE_HPP
