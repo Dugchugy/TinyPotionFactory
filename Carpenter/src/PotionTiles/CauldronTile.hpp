dc0e4fe877cca043255bac6bd67f650d38c2bc9b
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-08-12 08:36:07 -0700

    added constructor and type overrides

--- Carpenter/src/PotionTiles/CauldronTile.hpp
+++ Carpenter/src/PotionTiles/CauldronTile.hpp
@@ -2,10 +2,16 @@
 #define CAULDRONTILE_HPP
 
 #include "TileObject.hpp"
+#include "TilePosition.hpp"
+#include "TileIdentifier.hpp"
 
 namespace PotionGrid {
 
 class CauldronTile : public TileObject {
+
+   CauldronTile( TilePosition pos );
+
+   virtual TileType type();
 
 }; //CauldronTile 
 
