20d4828f0c7c47467d02bc5a7ac3f1b849f34d8e
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-08-14 20:33:23 -0700

    added documentation comments

--- Carpenter/src/PotionTiles/CauldronTile.hpp
+++ Carpenter/src/PotionTiles/CauldronTile.hpp
@@ -9,8 +9,15 @@ namespace PotionGrid {
 
 class CauldronTile : public TileObject {
 
+   /**
+    * @brief constructs a new cauldron at the specified position 
+    * @param pos the position of the new cauldron
+    */
    CauldronTile( TilePosition pos );
 
+   /**
+    * @brief returns that this is a cauldron
+    */
    virtual TileType type();
 
 }; //CauldronTile 
