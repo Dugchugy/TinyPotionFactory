eac6542991110d7b58b9729a1fe9cc809aa5c0b6
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2026-01-10 18:51:14 -0800

    added function headers for GetCameraLocationonPlane

--- Carpenter/src/GameObjects/CameraExtras.hpp
+++ Carpenter/src/GameObjects/CameraExtras.hpp
@@ -1,4 +1,25 @@
 #ifndef CAMERAEXTRAS_HPP
 #define CAMERAEXTRAS_HPP
 
+#include "Utils.hpp"
+#include "Camera.hpp"
+
+namespace PotionParts {
+
+    Engine::Vec3f GetCameraLocationOnPlane(
+        const Engine::Camera& cam,
+        const Engine::Vec2f& mouse_position,
+        const Engine::Vec3f& perpendicular,
+        const Engine::Vec3f& containts
+    );
+
+    Engine::Vec3f GetCameraLocationOnPlane(
+        const Engine::Camera& cam,
+        const Engine::Vec2f& mouse_position
+    ) {
+        return GetCameraLocationOnPlane( cam, mouse_position, {0, 0, 1}, {0, 0, 0});
+    }
+
+};
+
 #endif //CAMERAEXTRAS_HPP
